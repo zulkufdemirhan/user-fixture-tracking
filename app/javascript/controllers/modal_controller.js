0017175171a33eafb4e7e8fb4714fbe4a5b26697
Zülküf Demirhan <95721351+zulkufdemirhan@users.noreply.github.com>
2023-12-17 02:18:30 +0300

    turbo modal controller changes

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,25 +1,14 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  connect() {
-    document.addEventListener("keydown", this.handleKeydown);
-  }
-
-  disconnect() {
-    document.removeEventListener("keydown", this.handleKeydown);
-  }
-
   handleKeydown = (event) => {
-    if (event.key === "Escape") {
+    if (event.code === "Escape") {
       this.close();
     }
   }
 
   close() {
-    const modal = document.getElementById("modal");
-
-    modal.innerHTML = "";
-    modal.removeAttribute("src");
-    modal.removeAttribute("complete");
+    this.element.parentElement.removeAttribute("src");
+    this.element.remove();
   }
 }
